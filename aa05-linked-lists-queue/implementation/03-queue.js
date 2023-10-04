9136daf66c4cba9612ea8c5e6f66f7d603b2a6c8
TuckerHoneycutt <tuckerpersonalmail@gmail.com>
2023-10-04 17:27:46 -0500

    Third problem done

--- aa05-linked-lists-queue/implementation/03-queue.js
+++ aa05-linked-lists-queue/implementation/03-queue.js
@@ -9,17 +9,33 @@ class Queue {
     }
 
     enqueue(val) {
-        // Add node to end of queue (linked list)
+        const newNode = new SinglyLinkedNode(val);
 
-        // Your code here 
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length ++;
+        return this.length;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     dequeue() {
-        // Remove node from front of queue (linked list)
+        if(!this.head){
+            return null;
+        }
+        const removedNode = this.head;
+        this.head = this.head.next;
 
-        // Your code here 
+        if(!this.head){
+            this.tail = null;
+        }
+        this.length --;
+        return removedNode.value;
 
         // Write your hypothesis on the time complexity of this method here
     }
